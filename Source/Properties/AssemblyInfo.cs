28d53328eec18dee435836dc2b384a1935dd5589
DMagic <david.grandy@gmail.com>
2014-07-03 23:08:24 -0400

    KSPAssembly

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -19,15 +19,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5428988e-53a6-4d8e-8af4-014572513e22")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.1.0")]
-[assembly: AssemblyFileVersion("0.8.1.0")]
+[assembly: AssemblyVersion("0.8.2.0")]
+[assembly: AssemblyFileVersion("0.8.2.0")]
+[assembly: KSPAssembly("DMagic", 0, 82)]
