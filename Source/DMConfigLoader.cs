eace57425ff7267d75996ebd57355d046946fb8b
DMagic <david.grandy@gmail.com>
2014-07-23 15:46:37 -0400

    Load contract descriptions from config

--- Source/DMConfigLoader.cs
+++ Source/DMConfigLoader.cs
@@ -1,6 +1,34 @@
-﻿
-using System;
-using System.Collections.Generic;
+﻿#region license
+/* DMagic Orbital Science - DMConfigLoader
+ * Monobehaviour to load science contract paramaters at startup
+ *
+ * Copyright (c) 2014, David Grandy <david.grandy@gmail.com>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ * this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation and/or other materials 
+ * provided with the distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used 
+ * to endorse or promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  
+ */
+#endregion
+
 using UnityEngine;
 
 namespace DMagic
@@ -54,6 +82,18 @@ namespace DMagic
 				}
 			}
 			DMUtils.Logging("Successfully Added {0} New Experiments To Contract List", DMUtils.availableScience.Count);
+			foreach (ConfigNode node in GameDatabase.Instance.GetConfigNodes("DM_SCIENCE_STORY_DEF"))
+			{
+				foreach (ConfigNode storyNode in node.GetNodes("DM_SCIENCE_BACKSTORY"))
+				{
+					foreach (string story in storyNode.GetValues("generic"))
+					{
+						if (!string.IsNullOrEmpty(story))
+							DMUtils.storyList.Add(story);
+					}
+				}
+			}
+			DMUtils.Logging("Successfully Added {0} New Backstories to Story List", DMUtils.storyList.Count);
 		}
 
 		private void OnDestroy()
