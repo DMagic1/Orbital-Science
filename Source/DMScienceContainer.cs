156a0588b96beb3086570637c4e35e01a846d667
DMagic <david.grandy@gmail.com>
2014-07-27 23:14:11 -0400

    Moved science container class

---
+++ Source/DMScienceContainer.cs
@@ -0,0 +1,59 @@
+﻿#region license
+/* DMagic Orbital Science - DMScienceContainer
+ * Object to store experiment properties
+ *
+ * Copyright (c) 2014, David Grandy <david.grandy@gmail.com>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ * this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation and/or other materials 
+ * provided with the distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used 
+ * to endorse or promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  
+ */
+#endregion
+
+namespace DMagic
+{
+	internal class DMScienceContainer
+	{
+		internal int sitMask, bioMask;
+		internal DMScienceType type;
+		internal ScienceExperiment exp;
+		internal string sciPart, agent, name;
+
+		internal DMScienceContainer(string expName, ScienceExperiment sciExp, int sciSitMask, int sciBioMask, DMScienceType Type,  string sciPartID, string agentName)
+		{
+			name = expName;
+			sitMask = sciSitMask;
+			bioMask = sciBioMask;
+			exp = sciExp;
+			sciPart = sciPartID;
+			agent = agentName;
+			type = Type;
+		}
+	}
+
+	internal enum DMScienceType
+	{
+		Surface = 1,
+		Aerial = 2,
+		Space = 4
+	}
+}
