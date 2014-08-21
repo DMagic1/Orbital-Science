e67d468fa27da3649f4552ee47474fea8442704d
DMagic <david.grandy@gmail.com>
2014-08-21 16:16:49 -0400

    One location for all of the anomaly stuff

---
+++ Source/DMAnomalyList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace DMagic
+{
+	[KSPAddonImproved(KSPAddonImproved.Startup.TimeElapses, false)]
+	internal class DMAnomalyList : MonoBehaviour
+	{
+		internal static List<DMAnomalyObject> anomObjects = new List<DMAnomalyObject>();
+		private static bool scannerUpdating;
+		private static bool magUpdating;
+
+		private void Start()
+		{
+			GameEvents.onVesselSOIChanged.Add(SOIChange);
+			pqsBuild(FlightGlobals.currentMainBody);
+		}
+
+		private void OnDestroy()
+		{
+			GameEvents.onVesselSOIChanged.Remove(SOIChange);
+		}
+
+		internal static bool ScannerUpdating
+		{
+			get { return scannerUpdating; }
+			set { scannerUpdating = value; }
+		}
+
+		internal static bool MagUpdating
+		{
+			get { return magUpdating; }
+			set { magUpdating = value; }
+		}
+
+		private void SOIChange(GameEvents.HostedFromToAction<Vessel, CelestialBody> VB)
+		{
+			pqsBuild(VB.to);
+		}
+
+		private void pqsBuild(CelestialBody body)
+		{
+			anomObjects.Clear();
+			PQSCity[] Cities = FindObjectsOfType(typeof(PQSCity)) as PQSCity[];
+			foreach (PQSCity anomalyObject in Cities)
+			{
+				if (anomalyObject.transform.parent.name == FlightGlobals.currentMainBody.name)
+					anomObjects.Add(new DMAnomalyObject(anomalyObject));
+			}
+		}
+
+		internal static void updateAnomaly(Vessel v, DMAnomalyObject a)
+		{
+			Vector3d vPos = v.transform.position;
+			double vAlt = v.mainBody.GetAltitude(vPos);
+			a.Vdistance = (a.worldLocation - vPos).magnitude;
+			a.Vheight = Math.Abs(vAlt - a.alt);
+			a.Vhorizontal = Math.Sqrt((a.Vdistance * a.Vdistance) + (a.Vheight * a.Vheight));
+		}
+
+		internal static void bearing(Vessel v, DMAnomalyObject a)
+		{
+			double vlat = v.latitude;
+			double vlon = v.longitude;
+			double longdiff = (a.lon - vlon) * Mathf.Deg2Rad;
+			double y = Math.Sin(longdiff) * Math.Cos(Mathf.Deg2Rad * a.lat);
+			double x = Math.Cos(Mathf.Deg2Rad * vlat) * Math.Sin(Mathf.Deg2Rad * a.lat) - Math.Sin(Mathf.Deg2Rad * vlat) * Math.Cos(Mathf.Deg2Rad * a.lat) * Math.Cos(longdiff);
+			double aBearing = (Math.Atan2(y, x) * Mathf.Rad2Deg + 360) % 360;
+			a.bearing = aBearing;
+		}
+	}
+}
---
+++ Source/DMAnomalyObject.cs
@@ -0,0 +1,41 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace DMagic
+{
+	internal class DMAnomalyObject
+	{
+		internal PQSCity city;
+		internal Vector3d worldLocation;
+		internal CelestialBody body;
+		internal double lat, lon, alt;
+		internal double Vdistance, Vheight, Vhorizontal;
+		internal double bearing;
+		internal string name;
+
+		internal DMAnomalyObject(PQSCity City)
+		{
+			city = City;
+			name = city.name;
+			try
+			{
+				body = FlightGlobals.Bodies.FirstOrDefault(b => b.name == city.transform.parent.name);
+			}
+			catch (Exception e)
+			{
+				DMUtils.Logging("Something Went Wrong Here: {0}", e);
+			}
+			if (body != null)
+			{
+				worldLocation = city.transform.position;
+				lat = body.GetLatitude(worldLocation);
+				lon = body.GetLongitude(worldLocation);
+				alt = body.GetAltitude(worldLocation);
+			}
+		}
+	}
+}
