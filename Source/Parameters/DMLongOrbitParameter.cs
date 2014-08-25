db61ceac2e35ea76989183ef0709a1245d89bc4a
DMagic <david.grandy@gmail.com>
2014-08-25 16:38:33 -0400

    notes

--- Source/Parameters/DMLongOrbitParameter.cs
+++ Source/Parameters/DMLongOrbitParameter.cs
@@ -84,7 +84,7 @@ namespace DMagic
 
 		protected override string GetNotes()
 		{
-			return "Make sure that your vessel or vessels are equipped with both magnetometer and RPWS instruments while matching the specified orbital parameters";
+			return "Your vessel, or vessels, must be equipped with both magnetometer and RPWS instruments; vessels do not need to remain active throughout the period specified";
 		}
 
 		protected override void OnSave(ConfigNode node)
