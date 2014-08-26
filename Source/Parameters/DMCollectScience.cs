30677872591dd72158f7835f43437c8d5ee371ca
DMagic <david.grandy@gmail.com>
2014-08-26 17:18:10 -0400

    Formating

--- Source/Parameters/DMCollectScience.cs
+++ Source/Parameters/DMCollectScience.cs
@@ -44,7 +44,7 @@ namespace DMagic
 		private DMScienceContainer scienceContainer;
 		private DMAnomalyContract anomContract;
 		private string subject, name, biomeName, partName;
-		private int type; //type 0: standard experiment; type 1: standard survey; type 2: asteroid survey; type 3: anomaly
+		private int type; //type 0: standard survey; type 1: biological survey; type 2: anomaly
 
 		public DMCollectScience()
 		{
@@ -119,14 +119,14 @@ namespace DMagic
 
 		protected override string GetNotes()
 		{
-			if (type == 3)
-				return string.Format("Locate the anomalous signal coming from roughly {0} degrees {1} and {2} degrees {3}", anomContract.Lat, anomContract.CardNS, anomContract.Lon, anomContract.CardEW);
+			if (type == 2)
+				return string.Format("Locate the anomalous signal coming from roughly {0}° {1} and {2}° {3}", Math.Abs(anomContract.Lat), anomContract.CardNS, Math.Abs(anomContract.Lon), anomContract.CardEW);
 			else return base.GetNotes();
 		}
 
 		protected override string GetTitle()
 		{
-			if (type == 3)
+			if (type == 2)
 			{
 				if (scienceLocation == ExperimentSituations.SrfLanded)
 					return string.Format("{0} data from the surface near the anomalous signal", scienceContainer.exp.experimentTitle);
@@ -174,14 +174,14 @@ namespace DMagic
 		protected override void OnRegister()
 		{
 			GameEvents.OnScienceRecieved.Add(scienceReceive);
-			if (type == 3)
+			if (type == 2)
 				DMUtils.OnAnomalyScience.Add(anomalyReceive);
 		}
 
 		protected override void OnUnregister()
 		{
 			GameEvents.OnScienceRecieved.Remove(scienceReceive);
-			if (type == 3)
+			if (type == 2)
 				DMUtils.OnAnomalyScience.Remove(anomalyReceive);
 		}
 
@@ -217,65 +217,41 @@ namespace DMagic
 				DMUtils.Logging("Failed To Load Variables; Parameter Removed");
 				this.Root.RemoveParameter(this);
 			}
-			if (type == 3)
+			if (type == 2)
 				anomContract = (DMAnomalyContract)this.Root;
 			subject = string.Format("{0}@{1}{2}{3}", scienceContainer.exp.id, body.name, scienceLocation, biomeName.Replace(" ", ""));
 		}
 
-		private void anomalyReceive(CelestialBody Body, string exp, string name)
+		private void anomalyReceive(CelestialBody Body, string exp, string biome)
 		{
-			if (body == Body && exp == scienceContainer.exp.id && biomeName.Replace(" ", "") == name)
+			if (body == Body && exp == scienceContainer.exp.id && biomeName.Replace(" ", "") == biome)
 				ScreenMessages.PostScreenMessage("Results from Anomalous Signal recovered", 6f, ScreenMessageStyle.UPPER_CENTER);
 		}
 
 		private void scienceReceive(float sci, ScienceSubject sub)
 		{
-			DMUtils.DebugLog("New Science Results Collected With ID: {0}", sub.id);
-			DMUtils.DebugLog("Comparing To Target Science With ID: {0}", subject);
-			if (type == 0 || type == 3)
+			if (type == 0 || type == 2)
 			{
-				DMUtils.DebugLog("Checking Science Results For Type [{0}] Contract", type);
 				if (!string.IsNullOrEmpty(biomeName))
 				{
 					if (sub.id == subject)
-					{
-						DMUtils.DebugLog("Contract Complete");
 						base.SetComplete();
-					}
 				}
 				else
 				{
 					if (sub.id.Contains(subject))
 					{
-						if (sci < (scienceContainer.exp.baseValue * scienceContainer.transmit * sub.subjectValue * 0.4f))
+						if (sub.science < (scienceContainer.exp.baseValue * scienceContainer.transmit * sub.subjectValue * 0.3f))
 							ScreenMessages.PostScreenMessage("This area has already been studied, try investigating another region to complete the contract", 8f, ScreenMessageStyle.UPPER_CENTER);
 						else
-						{
-							DMUtils.DebugLog("Contract Complete");
 							base.SetComplete();
-						}
 					}
 				}
 			}
 			else if (type == 1)
 			{
-				DMUtils.DebugLog("Checking Science Results For Type [{0}] Contract", type);
-				if (!string.IsNullOrEmpty(biomeName))
-				{
-					if (sub.id == subject)
-					{
-						DMUtils.DebugLog("Contract Complete");
-						base.SetComplete();
-					}
-				}
-				else
-				{
-					if (sub.id.Contains(subject))
-					{
-						DMUtils.DebugLog("Contract Complete");
-						base.SetComplete();
-					}
-				}
+				if (sub.id.Contains(subject))
+					base.SetComplete();
 			}
 		}
 
