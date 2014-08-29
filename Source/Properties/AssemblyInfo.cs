5b7a84083dbc416fef007cd0e79423dd808bd549
DMagic <david.grandy@gmail.com>
2014-08-28 20:12:21 -0400

    Info version

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -21,4 +21,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.8.5.0")]
 [assembly: AssemblyFileVersion("0.8.5.0")]
+[assembly: AssemblyInformationalVersion("v0.85")]
 [assembly: KSPAssembly("DMagic", 0, 85)]
