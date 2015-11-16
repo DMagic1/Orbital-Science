bd5815db8b0c1bd783f4a710c07c4f5a89acc533
DMagic <david.grandy@gmail.com>
2015-11-16 01:27:13 -0500

    New modules for seismic experiment

---
+++ Source/DMSeismicHandler.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using DMagic.Part_Modules;
+using UnityEngine;
+
+namespace DMagic
+{
+
+	[KSPAddon(KSPAddon.Startup.Flight, false)]
+	public class DMSeismicHandler : MonoBehaviour
+	{
+		private DMSeismicHandler instance;
+
+		private Dictionary<uint, DMSeismicSensor> seismometers = new Dictionary<uint, DMSeismicSensor>();
+		private Dictionary<uint, DMSeismicHammer> hammers = new Dictionary<uint, DMSeismicHammer>();
+
+
+
+		private void Start()
+		{
+			instance = this;
+
+			StartCoroutine(loadSensors());
+		}
+
+		private void OnDestroy()
+		{
+
+		}
+
+		private void Update()
+		{
+
+		}
+
+		private IEnumerator loadSensors()
+		{
+			while (!FlightGlobals.ready)
+				yield return null;
+
+			for (int i = 0; i < FlightGlobals.Vessels.Count; i++)
+			{
+				Vessel v = FlightGlobals.Vessels[i];
+
+				if (v == null)
+					continue;
+
+				if (v.mainBody != FlightGlobals.ActiveVessel.mainBody)
+					continue;
+
+				if (v.loaded)
+				{
+					for (int j = 0; j < v.Parts.Count; j++)
+					{
+						Part p = v.Parts[j];
+
+						if (p == null)
+							continue;
+
+						DMSeismicSensor sensor = p.FindModuleImplementing<DMSeismicSensor>();
+
+						if (sensor != null)
+							addSeismometer(p.flightID, sensor);
+
+						DMSeismicHammer hammer = p.FindModuleImplementing<DMSeismicHammer>();
+
+						if (hammer != null)
+							addHammer(p.flightID, hammer);
+					}
+				}
+				else
+				{
+					ProtoVessel pv = v.protoVessel;
+
+					for (int j = 0; j < v.protoVessel.protoPartSnapshots.Count; j++)
+					{
+						ProtoPartSnapshot p = v.protoVessel.protoPartSnapshots[j];
+
+						if (p == null)
+							continue;
+
+						for (int k = 0; k < p.modules.Count; k++)
+						{
+							ProtoPartModuleSnapshot m = p.modules[k];
+
+							if (m == null)
+								continue;
+
+							if (m.moduleName == "DMSeismicSensor")
+							{
+								if (m.moduleRef.GetType() == typeof(DMSeismicSensor))
+								{
+									addSeismometer(p.flightID, (DMSeismicSensor)m.moduleRef);
+									continue;
+								}
+							}
+
+							if (m.moduleName == "DMSeismicHammer")
+							{
+								if (m.moduleRef.GetType() == typeof(DMSeismicHammer))
+								{
+									addHammer(p.flightID, (DMSeismicHammer)m.moduleRef);
+									continue;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		private void addSeismometer(uint id, DMSeismicSensor sensor)
+		{
+			if (!seismometers.ContainsKey(id))
+				seismometers.Add(id, sensor);
+		}
+
+		private void addHammer(uint id, DMSeismicHammer hammer)
+		{
+			if (!hammers.ContainsKey(id))
+				hammers.Add(id, hammer);
+		}
+		
+
+		public DMSeismicHandler Instance
+		{
+			get { return instance; }
+		}
+
+	}
+}
---
+++ Source/Part Modules/DMSeismicSensor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace DMagic.Part_Modules
+{
+	public class DMSeismicSensor : DMBasicScienceModule
+	{
+		[KSPField]
+		public string animationName = "";
+
+
+		private Animation Anim;
+
+		public override void OnStart(PartModule.StartState state)
+		{
+			if (!string.IsNullOrEmpty(animationName))
+				Anim = part.FindModelAnimators(animationName)[0];
+			if (!string.IsNullOrEmpty(experimentID))
+				exp = ResearchAndDevelopment.GetExperiment(experimentID);
+		}
+
+		public override void OnLoad(ConfigNode node)
+		{
+			base.OnLoad(node);
+		}
+
+		public override void OnSave(ConfigNode node)
+		{
+			base.OnSave(node);
+		}
+
+		private void Update()
+		{
+			base.EventsCheck();
+		}
+
+		#region Animator
+
+		//Controls the main, door-opening animation
+		private void animator(float speed, float time, Animation a, string name)
+		{
+			if (a != null)
+			{
+				a[name].speed = speed;
+				if (!a.IsPlaying(name))
+				{
+					a[name].normalizedTime = time;
+					a.Blend(name, 1f);
+				}
+			}
+		}
+
+
+		#endregion
+
+	}
+}
