92deb374edfc7c339fd3fd6209a9c3cef9aceaba
DMagic <david.grandy@gmail.com>
2016-01-01 20:33:12 -0500

    New recon contract

---
+++ Source/Contracts/DMReconContract.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Contracts;
+using Contracts.Parameters;
+using Contracts.Agents;
+using DMagic.Parameters;
+using FinePrint;
+using FinePrint.Utilities;
+using FinePrint.Contracts.Parameters;
+
+namespace DMagic.Contracts
+{
+	public class DMReconContract : Contract
+	{
+		private CelestialBody body;
+		private System.Random rand = DMUtils.rand;
+
+		protected override bool Generate()
+		{
+			DMReconContract[] reconContracts = ContractSystem.Instance.GetCurrentContracts<DMReconContract>();
+			int offers = 0;
+			int active = 0;
+			int maxOffers = DMContractDefs.DMRecon.maxOffers;
+			int maxActive = DMContractDefs.DMRecon.maxActive;
+
+			for (int i = 0; i < reconContracts.Length; i++)
+			{
+				DMReconContract r = reconContracts[i];
+				if (r.ContractState == State.Offered)
+					offers++;
+				else if (r.ContractState == State.Active)
+					active++;
+			}
+
+			if (offers >= maxOffers)
+				return false;
+			if (active >= maxActive)
+				return false;
+
+			List<CelestialBody> customReachedBodies = new List<CelestialBody>();
+
+			switch(prestige)
+			{
+				case ContractPrestige.Trivial:
+					customReachedBodies.AddRange(GetBodies_Reached(true, false));
+					customReachedBodies.AddRange(GetBodies_NextUnreached(2, null));
+					customReachedBodies.RemoveAll(b => b.pqsController == null);
+
+					body = customReachedBodies[rand.Next(0, customReachedBodies.Count)];
+					break;
+				case ContractPrestige.Significant:
+					customReachedBodies = ContractSystem.Instance.GetCompletedContracts<DMReconContract>().Where(r => r.prestige == ContractPrestige.Trivial).Select(r => r.body).ToList();
+
+					body = customReachedBodies[rand.Next(0, customReachedBodies.Count)];
+					break;
+				case ContractPrestige.Exceptional:
+					customReachedBodies = ContractSystem.Instance.GetCompletedContracts<DMReconContract>().Where(r => r.prestige == ContractPrestige.Significant).Select(r => r.body).ToList();
+
+					body = customReachedBodies[rand.Next(0, customReachedBodies.Count)];
+					break;
+			}
+
+			if (body == null)
+				return false;
+
+			double time = 0;
+
+			OrbitType orbitType = OrbitType.POLAR;
+			double deviation = 10;
+
+			Dictionary<int, List<string>> parts = new Dictionary<int, List<string>>();
+			Orbit o = new Orbit();
+
+			switch(prestige)
+			{
+				case ContractPrestige.Trivial:
+					parts.Add(0, DMContractDefs.DMRecon.reconTrivialParts);
+					if (!DMUtils.partAvailable(DMContractDefs.DMRecon.reconTrivialParts))
+						return false;
+					o = CelestialUtilities.GenerateOrbit(orbitType, this.MissionSeed, body, 0.5, ContractDefs.Satellite.TrivialInclinationDifficulty);
+					time = 1080000d * (double)(prestige + 1) * ((double)rand.Next(6, 17) / 10d);
+					break;
+				case ContractPrestige.Significant:
+					parts.Add(0, DMContractDefs.DMRecon.reconSignificantParts);
+					if (!DMUtils.partAvailable(DMContractDefs.DMRecon.reconSignificantParts))
+						return false;
+					if (SystemUtilities.CoinFlip(rand))
+						orbitType = OrbitType.KOLNIYA;
+					else
+						orbitType = OrbitType.TUNDRA;
+					o = CelestialUtilities.GenerateOrbit(orbitType, this.MissionSeed, body, 0.5, ContractDefs.Satellite.TrivialInclinationDifficulty);time = 3240000d * (double)(prestige + 1) * ((double)rand.Next(6, 17) / 10d);
+					break;
+				case ContractPrestige.Exceptional:
+					parts.Add(0, DMContractDefs.DMRecon.reconExceptionalParts);
+					if (!DMUtils.partAvailable(DMContractDefs.DMRecon.reconExceptionalParts))
+						return false;
+					o = CelestialUtilities.GenerateOrbit(orbitType, this.MissionSeed, body, 0.5, ContractDefs.Satellite.TrivialInclinationDifficulty);
+					time = 2160000d * (double)(prestige + 1) * ((double)rand.Next(6, 17) / 10d);
+					break;
+			}
+
+			DMLongOrbitParameter longOrbit = new DMLongOrbitParameter(time);
+			DMPartRequestParameter partRequest = new DMPartRequestParameter(parts, body);
+			DMReconOrbitParameter reconParam = new DMReconOrbitParameter(orbitType, o.inclination, o.eccentricity, o.semiMajorAxis, o.LAN, o.argumentOfPeriapsis, o.meanAnomalyAtEpoch, o.epoch, body, ContractDefs.Satellite.SignificantDeviation, longOrbit);
+
+			longOrbit.AddParameter(reconParam);
+			longOrbit.AddParameter(partRequest);
+
+			reconParam.AddParameter(new DMSpecificOrbitParameterExtended(orbitType, o.inclination, o.eccentricity, o.semiMajorAxis, o.LAN, o.argumentOfPeriapsis, o.meanAnomalyAtEpoch, o.epoch, body, deviation));
+
+			if (this.ParameterCount == 0)
+				return false;
+
+			float primaryModifier = ((float)rand.Next(80, 121) / 100f);
+			float diffModifier = 1 + ((float)this.Prestige * 0.5f);
+
+			this.agent = AgentList.Instance.GetAgent("DMagic");
+
+			if (this.agent == null)
+				this.agent = AgentList.Instance.GetAgentRandom();
+
+			base.SetExpiry(10 * DMUtils.deadline, 20f * DMUtils.deadline);
+			base.SetDeadlineDays((float)(time / KSPUtil.KerbinDay) * 3.9f * (this.GetDestinationWeight(body) / 1.8f) * DMUtils.deadline * primaryModifier, null);
+			base.SetReputation(8f * diffModifier * DMUtils.reward * primaryModifier, 7f * diffModifier * DMUtils.penalty * primaryModifier, null);
+			base.SetFunds(35000 * diffModifier * DMUtils.forward * primaryModifier, 40000 * diffModifier * DMUtils.reward * primaryModifier, 28000 * diffModifier * DMUtils.penalty * primaryModifier, body);
+			base.SetScience(15f * diffModifier * DMUtils.science * primaryModifier, body);
+			return true;
+		}
+
+		public override bool MeetRequirements()
+		{
+			return ProgressTracking.Instance.NodeComplete(new string[] { Planetarium.fetch.Home.name, "Orbit" });
+		}
+
+		public override bool CanBeCancelled()
+		{
+			return true;
+		}
+
+		public override bool CanBeDeclined()
+		{
+			return true;
+		}
+
+		protected override string GetDescription()
+		{
+			string story = DMContractDefs.DMRecon.backStory[rand.Next(0, DMContractDefs.DMRecon.backStory.Count)];
+			return string.Format(story, this.agent.Name, body.theName);
+		}
+
+		protected override string GetHashString()
+		{
+			return string.Format("{0}{1}", body.name, prestige);
+		}
+
+		protected override string GetNotes()
+		{
+			return base.GetNotes();
+		}
+
+		protected override string GetSynopsys()
+		{
+			switch(prestige)
+			{
+				case ContractPrestige.Trivial:
+					return "";
+				case ContractPrestige.Significant:
+					return "";
+				default:
+					return "";
+			}
+		}
+
+		protected override string GetTitle()
+		{
+			string t = "";
+
+			switch (prestige)
+			{
+				case ContractPrestige.Trivial:
+					t = "orbital";
+					break;
+				case ContractPrestige.Significant:
+					t = "radio signals";
+					break;
+				case ContractPrestige.Exceptional:
+					t = "stereographic orbital";
+					break;
+			}
+
+			return string.Format("Conduct a long term {0} reconnaissance survey of {1}", t, body.theName);
+		}
+
+		protected override string MessageCompleted()
+		{
+			switch (prestige)
+			{
+				case ContractPrestige.Trivial:
+					return string.Format("");
+				case ContractPrestige.Significant:
+					return string.Format("");
+				default:
+					return string.Format("");
+			}
+		}
+
+		protected override void OnLoad(ConfigNode node)
+		{
+			body = node.parse("Recon_Target", (CelestialBody)null);
+
+			if (body == null)
+			{
+				DMUtils.Logging("Error while loading Recon Contract target body; removing contract now...");
+				this.Unregister();
+				ContractSystem.Instance.Contracts.Remove(this);
+				return;
+			}
+
+			if (this.ParameterCount == 0)
+			{
+				DMUtils.Logging("No Parameters Loaded For This Recon Contract; Removing Now...");
+				this.Unregister();
+				ContractSystem.Instance.Contracts.Remove(this);
+				return;
+			}
+		}
+
+		protected override void OnSave(ConfigNode node)
+		{
+			node.AddValue("Recon_Target", body.flightGlobalsIndex);
+		}
+
+		/// <summary>
+		/// Used externally to return the target Celestial Body
+		/// </summary>
+		/// <param name="cP">Instance of the requested Contract</param>
+		/// <returns>Celestial Body object</returns>
+		public static CelestialBody TargetBody(Contract c)
+		{
+			if (c == null || c.GetType() != typeof(DMReconContract))
+				return null;
+
+			try
+			{
+				DMReconContract Instance = (DMReconContract)c;
+				return Instance.body;
+			}
+			catch (Exception e)
+			{
+				Debug.LogError("Error while accessing DMagic Recon Contract Target Body\n" + e);
+				return null;
+			}
+		}
+
+	}
+}
---
+++ Source/Parameters/DMReconOrbitParameter.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using DMagic.Contracts;
+using Contracts;
+using FinePrint.Utilities;
+using FinePrint.Contracts.Parameters;
+
+namespace DMagic.Parameters
+{
+	public class DMReconOrbitParameter : ContractParameter
+	{
+		private DMSpecificOrbitParameterExtended childOrbitParameter;
+		private CelestialBody body;
+		OrbitType type;
+		double inc;
+		double ecc;
+		double sma;
+		double lan;
+		double aop;
+		double mae;
+		double epo;
+		double deviation;
+		OrbitDriver orbitDriver;
+		private DMLongOrbitParameter root;
+
+		public DMReconOrbitParameter() { }
+
+		public DMReconOrbitParameter(OrbitType orbitType, double inclination, double eccentricity, double semi, double la, double argumentOfPeriapsis, double meanAnomalyAtEpoch, double epoch, CelestialBody targetBody, double deviationWindow, DMLongOrbitParameter r)
+		{
+			type = orbitType;
+			body = targetBody;
+			inc = inclination;
+			ecc = eccentricity;
+			sma = semi;
+			lan = la;
+			aop = argumentOfPeriapsis;
+			mae = meanAnomalyAtEpoch;
+			epo = epoch;
+			deviation = deviationWindow;
+			root = r;
+			disableOnStateChange = false;
+			setupOrbit();
+		}
+
+		protected override string GetTitle()
+		{
+			return childOrbitParameter.BaseTitle;
+		}
+
+		protected override string GetNotes()
+		{
+			return childOrbitParameter.BaseNotes + "\nNote that the target orbit will not disappear until the full contract has been completed.";
+		}
+
+		private void setupOrbit()
+		{
+			orbitDriver = new OrbitDriver();
+			orbitDriver.orbit = new Orbit();
+			orbitDriver.orbit.referenceBody = body;
+			orbitDriver.orbit.inclination = inc;
+			orbitDriver.orbit.eccentricity = ecc;
+			orbitDriver.orbit.argumentOfPeriapsis = aop;
+			orbitDriver.orbit.semiMajorAxis = sma;
+			orbitDriver.orbit.LAN = lan;
+			orbitDriver.orbit.meanAnomalyAtEpoch = mae;
+			orbitDriver.orbit.epoch = epo;
+			orbitDriver.orbit.Init();
+		}
+
+		protected override void OnUpdate()
+		{
+			if (HighLogic.LoadedSceneIsEditor)
+				return;
+
+			if (orbitDriver == null)
+			{
+				this.SetIncomplete();
+				return;
+			}
+
+			if (orbitDriver.orbit == null)
+			{
+				this.SetIncomplete();
+				return;
+			}
+
+			if (root == null)
+			{
+				this.SetIncomplete();
+				return;
+			}
+
+			for (int i = 0; i < root.VesselCount; i++)
+			{
+				Vessel v = root.GetVessel(i);
+
+				if (v == null)
+					continue;
+
+				if (VesselUtilities.VesselAtOrbit(orbitDriver.orbit, deviation, v))
+				{
+					this.SetComplete();
+					return;
+				}
+			}
+
+			this.SetIncomplete();
+		}
+
+		protected override void OnLoad(ConfigNode node)
+		{
+			string[] values = node.GetValue("Recon_Values").Split('|');
+
+			if (values.Length != 10)
+			{
+				loadFail("Error while loading DMReconOrbitParameter; Removing Now...");
+				return;
+			}
+
+			int target;
+			if (int.TryParse(values[0], out target))
+			{
+				if (FlightGlobals.Bodies.Count >= target)
+					body = FlightGlobals.Bodies[target];
+				else
+				{
+					loadFail(string.Format("No Celestial Body Found For Index: [{0}] For Recon Orbit Parameter; Removing Now...", target));
+					return;
+				}
+			}
+			else
+			{
+				loadFail("");
+				return;
+			}
+
+			int t = DMUtils.parseValue(values[1], (int)2);
+			type = (OrbitType)t;
+
+			inc = DMUtils.parseValue(values[2], (double)75);
+			ecc = DMUtils.parseValue(values[3], (double)0);
+			sma = DMUtils.parseValue(values[4], (double)0);
+			aop = DMUtils.parseValue(values[5], (double)0);
+			mae = DMUtils.parseValue(values[6], (double)0);
+			epo = DMUtils.parseValue(values[7], (double)0);
+			lan = DMUtils.parseValue(values[8], (double)0);
+			deviation = DMUtils.parseValue(values[9], (double)10);
+
+			if (this.ParameterCount <= 0)
+			{
+				loadFail("");
+				return;
+			}
+
+			try
+			{
+				childOrbitParameter = (DMSpecificOrbitParameterExtended)this.GetParameter(0);
+			}
+			catch (Exception e)
+			{
+				loadFail("Could not find child specific orbit parameter; removing DMReconOrbit Parameter\n" + e.ToString());
+				return;
+			}
+
+			try
+			{
+				root = (DMLongOrbitParameter)Parent;
+			}
+			catch (Exception e)
+			{
+				loadFail("Could not find root long orbit parameter; removing DMReconOrbit Parameter\n" + e.ToString());
+				return;
+			}
+
+			disableOnStateChange = false;
+
+			setupOrbit();
+		}
+
+		protected override void OnSave(ConfigNode node)
+		{
+			node.AddValue("Recon_Values", string.Format("{0}|{1}|{2:N5}|{3:N5}|{4:N5}|{5:N5}|{6:N5}|{7:N5}|{8:N5}|{9:N5}", body.flightGlobalsIndex, (int)type, inc, ecc, sma, aop, mae, epo, lan, deviation));
+		}
+
+		private void loadFail(string message)
+		{
+			this.Unregister();
+			this.Parent.RemoveParameter(this);
+			DMUtils.Logging(message);
+		}
+
+	}
+}
