c03648de0aa9053c004aed7eab1484f6a76612e5
DMagic <david.grandy@gmail.com>
2016-01-01 20:33:41 -0500

    Dummy specific orbit parameter; should keep the orbit rendering while the contract is active

---
+++ Source/Parameters/DMSpecificOrbitParameterExtended.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Contracts;
+using FinePrint.Utilities;
+using FinePrint.Contracts.Parameters;
+
+namespace DMagic.Parameters
+{
+	public class DMSpecificOrbitParameterExtended : SpecificOrbitParameter
+	{
+		private string baseTitle = "";
+		private string baseNotes = "";
+
+		public DMSpecificOrbitParameterExtended() { }
+
+		public DMSpecificOrbitParameterExtended(OrbitType orbitType, double inclination, double eccentricity, double sma, double lan, double argumentOfPeriapsis, double meanAnomalyAtEpoch, double epoch, CelestialBody targetBody, double deviationWindow) : base(orbitType, inclination, eccentricity, sma, lan, argumentOfPeriapsis, meanAnomalyAtEpoch, epoch, targetBody, deviationWindow)
+		{
+			
+		}
+
+		protected override void OnUpdate()
+		{
+			if (base.state != ParameterState.Incomplete)
+				base.SetIncomplete();
+
+			return;
+		}
+
+		protected override string GetNotes()
+		{
+			baseNotes = base.GetNotes();
+
+			return "";
+		}
+
+		protected override string GetTitle()
+		{
+			baseTitle = base.GetTitle();
+
+			return "";
+		}
+
+		public string BaseTitle
+		{
+			get { return baseTitle; }
+		}
+
+		public string BaseNotes
+		{
+			get { return baseNotes; }
+		}
+	}
+}
