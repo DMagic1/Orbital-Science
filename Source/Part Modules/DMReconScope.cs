583560b4fcf8a8eaaf1eca07e2c256c5bc49fd4b
DMagic <david.grandy@gmail.com>
2016-01-17 20:49:14 -0500

    Looping animation and anomaly scan

--- Source/Part Modules/DMReconScope.cs
+++ Source/Part Modules/DMReconScope.cs
@@ -1,17 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 namespace DMagic.Part_Modules
 {
 	public class DMReconScope : DMModuleScienceAnimate, IDMSurvey
 	{
+		[KSPField]
+		public string loopingAnimName = "";
 
+		private Animation loopingAnim;
 
+		public override void OnStart(PartModule.StartState state)
+		{
+			base.OnStart(state);
+
+			if (!string.IsNullOrEmpty(loopingAnimName))
+				loopingAnim = part.FindModelAnimators(loopingAnimName)[0];
+		}
 
 		public void scanPlanet(CelestialBody b)
 		{
+			DMAnomalyStorage anom = DMAnomalyList.getAnomalyStorage(b.name);
 
+			if (anom == null)
+				anom = new DMAnomalyStorage(b, false);
+
+			if (anom.Scanned)
+				return;
+
+			if (anom.scanBody())
+				DMAnomalyList.addAnomalyStorage(b.name, anom);
 		}
 	}
 }
