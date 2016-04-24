2b08c84ead3253ff47303feeeae3f8b2085bb1de
DMagic <david.grandy@gmail.com>
2016-04-24 18:19:25 -0400

    version

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5428988e-53a6-4d8e-8af4-014572513e22")]
 
-[assembly: AssemblyVersion("1.2.0.3")]
-[assembly: AssemblyFileVersion("1.2.0.3")]
-[assembly: AssemblyInformationalVersion("v1.2.3")]
+[assembly: AssemblyVersion("1.2.0.4")]
+[assembly: AssemblyFileVersion("1.2.0.4")]
+[assembly: AssemblyInformationalVersion("v1.2.4")]
 [assembly: KSPAssembly("DMagic", 1, 4)]
