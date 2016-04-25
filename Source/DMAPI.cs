695a6ce3b71d56aa85cc648e4b507e9dd52ceade
DMagic <david.grandy@gmail.com>
2016-04-25 09:40:39 -0400

    API documentation

--- Source/DMAPI.cs
+++ Source/DMAPI.cs
@@ -41,6 +41,11 @@ namespace DMagic
 	public static class DMAPI
 	{
 
+		/// <summary>
+		/// Use to determine whether an experiment can be conducted at this time. This returns the same value as the internal check used when an experiment is deplyed from the right-click menu.
+		/// </summary>
+		/// <param name="isc">The science experiment module must be cast as an IScienceDataContianer.</param>
+		/// <returns>Returns true if the experiment can be performed; will return false if the science module is not of the right type.</returns>
 		public static bool experimentCanConduct(IScienceDataContainer isc)
 		{
 			Type t = isc.GetType();
@@ -89,6 +94,12 @@ namespace DMagic
 			return false;
 		}
 
+		/// <summary>
+		/// Uses the internal method for conducting an experiment; the experiment cannot be forced and must first pass the "canConduct". All associated animations and other functions will be called. Optinally run the experiment without opening the results window.
+		/// </summary>
+		/// <param name="isc">The science experiment module must be cast as an IScienceDataContianer.</param>
+		/// <param name="silent">Set to true to prevent the science results dialog from opening.</param>
+		/// <returns>Returns true if the science module is of the right type and the gather science method is called.</returns>
 		public static bool deployDMExperiment(IScienceDataContainer isc, bool silent = false)
 		{
 			Type t = isc.GetType();		
@@ -133,6 +144,11 @@ namespace DMagic
 			return false;
 		}
 
+		/// <summary>
+		/// Calls the internal method for getting the Experiment Situation for a certain experiment.
+		/// </summary>
+		/// <param name="mse">The science experiment module must be cast as a ModuleScienceExperiment.</param>
+		/// <returns>Returns the Experiment Situation value for that experiment; returns InSpaceHigh if the experiment is not of the right type.</returns>
 		public static ExperimentSituations getExperimentSituation(ModuleScienceExperiment mse)
 		{
 			Type t = mse.GetType();
@@ -166,6 +182,12 @@ namespace DMagic
 			return ExperimentSituations.InSpaceHigh;
 		}
 
+		/// <summary>
+		/// Calls the internal method for getting the biome for a certain experiment.
+		/// </summary>
+		/// <param name="mse">The science experiment module must be cast as a ModuleScienceExperiment.</param>
+		/// <param name="sit">The current Experiment Situation value; see getExperimentSituation.</param>
+		/// <returns>Returns the biome string for that experiment; returns an empty string if the experiment is not of the right type.</returns>
 		public static string getBiome(ModuleScienceExperiment mse, ExperimentSituations sit)
 		{
 			Type t = mse.GetType();
@@ -199,6 +221,11 @@ namespace DMagic
 			return "";
 		}
 
+		/// <summary>
+		/// Check if an experiment can be conducted on asteroids.
+		/// </summary>
+		/// <param name="mse">The science experiment module must be cast as a ModuleScienceExperiment.</param>
+		/// <returns>Returns false if the module is not of the right type or if the experiment cannot be conducted with asteroids.</returns>
 		public static bool isAsteroidExperiment(ModuleScienceExperiment mse)
 		{
 			Type t = mse.GetType();
@@ -210,27 +237,65 @@ namespace DMagic
 			return DMMod.asteroidReports;
 		}
 
+		/// <summary>
+		/// Check if an experiment can be conducted on asteroids.
+		/// </summary>
+		/// <param name="dms">The science experiment module must be cast as a DMModuleScienceAnimate.</param>
+		/// <returns>Returns false if the module is not of the right type or if the experiment cannot be conducted with asteroids.</returns>
 		public static bool isAsteroidExperiment(DMModuleScienceAnimate dms)
 		{
 			return dms.asteroidReports;
 		}
 
+		/// <summary>
+		/// Check to see if an asteroid is within loading distance.
+		/// </summary>
+		/// <returns>Returns true if an asteroid is detected within loading distance (2.5km)</returns>
+		public static bool isAsteroidNear()
+		{
+			return DMAsteroidScience.AsteroidNear;
+		}
+
+		/// <summary>
+		/// Check to see if an asteroid is within loading distance and an experiment is valid for asteroid science.
+		/// </summary>
+		/// <param name="mse">The science experiment module must be cast as a ModuleScienceExperiment.</param>
+		/// <returns>Returns false if the module is not of the right type, if the experiment cannot be conducted with asteroids, or if no asteroids are within loading distance (2.5km)</returns>
 		public static bool isAsteroidNear(ModuleScienceExperiment mse)
 		{
 			if (!isAsteroidExperiment(mse))
 				return false;
 
-			return DMAsteroidScience.AsteroidNear;
+			return isAsteroidNear();
 		}
 
+		/// <summary>
+		/// Check to see if an asteroid is grappled to the current vessel.
+		/// </summary>
+		/// <returns>Returns true if an asteroid is attached to the current vessel.</returns>
+		public static bool isAsteroidGrappled()
+		{
+			return DMAsteroidScience.AsteroidGrappled;
+		}
+
+		/// <summary>
+		/// Check to see if an asteroid is grappled to the current vessel and an experiment is valid for asteroid science
+		/// </summary>
+		/// <param name="mse">The science experiment module must be cast as a ModuleScienceExperiment.</param>
+		/// <returns>Returns false if the module is not of the right type, if the experiment cannot be conducted with asteroids, or if no asteroid is attached to the current vessel.</returns>
 		public static bool isAsteroidGrappled(ModuleScienceExperiment mse)
 		{
 			if (!isAsteroidExperiment(mse))
 				return false;
 
-			return DMAsteroidScience.AsteroidGrappled;
+			return isAsteroidGrappled();
 		}
 
+		/// <summary>
+		/// Get the ScienceSubject for an asteroid experiment.
+		/// </summary>
+		/// <param name="mse">The science experiment module must be cast as a ModuleScienceExperiment.</param>
+		/// <returns>Returns the ScienceSubject for that specific asteroid and experiment; returns null if the module is not of the right type, the experiment is not suitable for astroids, if no asteroids are detected, or if the current asteroid situation is not suitable for the experiment.</returns>
 		public static ScienceSubject getAsteroidSubject(ModuleScienceExperiment mse)
 		{
 			Type t = mse.GetType();
@@ -270,6 +335,12 @@ namespace DMagic
 			return null;
 		}
 
+		/// <summary>
+		/// Get the ScienceSubject for an asteroid experiment.
+		/// </summary>
+		/// <param name="mse">The science experiment module must be cast as a ModuleScienceExperiment.</param>
+		/// <param name="sit">The current Experiment Situation value; see the getExperimentSituation method above.</param>
+		/// <returns>Returns the ScienceSubject for that specific asteroid, experiment, and ExperimentSituation; returns null if the module is not of the right type, the experiment is not suitable for astroids, if no asteroids are detected, or if the current asteroid situation is not suitable for the experiment.</returns>
 		public static ScienceSubject getAsteroidSubject(ModuleScienceExperiment mse, ExperimentSituations sit)
 		{
 			Type t = mse.GetType();
@@ -285,6 +356,19 @@ namespace DMagic
 			if (DMMod.scienceExp == null)
 				return null;
 
+			if (sit == ExperimentSituations.InSpaceLow)
+			{
+				if (!isAsteroidNear())
+					return null;
+			}
+			else if (sit == ExperimentSituations.SrfLanded)
+			{
+				if (!isAsteroidGrappled())
+					return null;
+			}
+			else
+				return null;
+
 			if ((DMMod.scienceExp.situationMask & (int)sit) == 0)
 				return null;
 
@@ -294,6 +378,11 @@ namespace DMagic
 			return sub;
 		}
 
+		/// <summary>
+		/// Get the science amount recovered from a certain astroid type and experiment.
+		/// </summary>
+		/// <param name="title">The Science Subject title (not the ID) for the asteroid type and experiment.</param>
+		/// <returns>Returns the amount of science already recovered; does not included science that has not been transmitted or recovered; returns 0 if no results for this subjuect are found.</returns>
 		public static float getDMScienceRecoveredValue(string title)
 		{
 			if (DMScienceScenario.SciScenario == null)
@@ -307,6 +396,11 @@ namespace DMagic
 			return DMS.Science;
 		}
 
+		/// <summary>
+		/// Get the science amount recovered from a certain astroid type and experiment.
+		/// </summary>
+		/// <param name="sub">The Science Subject for the asteroid type and experiment.</param>
+		/// <returns>Returns the amount of science already recovered; does not included science that has not been transmitted or recovered; returns 0 if no results for this subjuect are found.</returns>
 		public static float getDMScienceRecoveredValue(ScienceSubject sub)
 		{
 			if (sub == null)
@@ -315,11 +409,22 @@ namespace DMagic
 			return getDMScienceRecoveredValue(sub.title);
 		}
 
+		/// <summary>
+		/// Get the science amount recovered from a certain astroid type and experiment. This method must first determine the science subject and will only work if the experiment is properly suited for asteroid science and there is an asteroid nearby or on the vessel.
+		/// </summary>
+		/// <param name="mse">The science experiment module must be cast as a ModuleScienceExperiment.</param>
+		/// <returns>Returns the amount of science already recovered; does not included science that has not been transmitted or recovered; returns 0 if no results for this subjuect are found or if the subject is not valid.</returns>
 		public static float getDMScienceRecoveredValue(ModuleScienceExperiment mse)
 		{
 			return getDMScienceRecoveredValue(getAsteroidSubject(mse));
 		}
 
+		/// <summary>
+		/// Get the science amount recovered from a certain astroid type and experiment. This method must first determine the science subject and will only work if the experiment is properly suited for asteroid science and there is an asteroid nearby or on the vessel.
+		/// </summary>
+		/// <param name="mse">The science experiment module must be cast as a ModuleScienceExperiment.</param>
+		/// <param name="sit">The current Experiment Situation value; see the getExperimentSituation method above.</param>
+		/// <returns>Returns the amount of science already recovered; does not included science that has not been transmitted or recovered; returns 0 if no results for this subjuect are found or if the subject is not valid.</returns>
 		public static float getDMScienceRecoveredValue(ModuleScienceExperiment mse, ExperimentSituations sit)
 		{
 			return getDMScienceRecoveredValue(getAsteroidSubject(mse, sit));
